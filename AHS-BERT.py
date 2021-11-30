ae5e91e1fb5e95574ab15d3bd13d3541bf4153fd
alexwmays <59802340+alexwmays@users.noreply.github.com>
2021-11-30 12:08:20 -0500

    Testing implemented and validation_end and test_end changed to correctly report the average error. Further, new layers and activation functions added

--- AHS-BERT.py
+++ AHS-BERT.py
@@ -1,33 +1,32 @@
-import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-import nltk
+import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-import transformers
-from transformers import AutoModel, BertTokenizer, BertModel, BertTokenizerFast
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
 from torch.utils.data import TensorDataset, DataLoader
-import sklearn.metrics as metrics
-import pytorch_lightning as pl
-from transformers import get_linear_schedule_with_warmup
+from transformers import BertModel, BertTokenizerFast
 
 
 class AHS_BERT(pl.LightningModule):
-    def __init__(self, drop_rate=0.2):
+    def __init__(self, drop_rate=0.2, freeze=True):
         super(AHS_BERT, self).__init__()
 
         #Pretrained model
         self.bert = BertModel.from_pretrained('bert-base-uncased')
 
+        #Freeze bert connected layers, drastically speed up training
+        if freeze is True:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+
         #Replacement Layer/Finetuner
         self.regressor = nn.Sequential(
             nn.Dropout(drop_rate),
-            nn.Linear(768, 1))
+            nn.Linear(768, 512),
+            nn.Linear(512, 1),
+            torch.nn.ReLU())
 
     def forward(self, input_ids, att_mask):
         vars = self.bert(input_ids=input_ids, attention_mask=att_mask)
@@ -42,8 +41,7 @@ class AHS_BERT(pl.LightningModule):
         y_pred = self.forward(input_ids, attention_mask)
         # loss
         loss = F.mse_loss(y_pred.float(), target.float())
-        tensorboard_logs = {'train_loss': loss}
-        return {'loss': loss, 'log': tensorboard_logs}
+        return loss
 
     def validation_step(self, batch, batch_idx):
         # batch
@@ -51,12 +49,12 @@ class AHS_BERT(pl.LightningModule):
         # fwd
         y_pred = self.forward(input_ids, attention_mask)
         # loss
-        return {'val_loss':  F.mse_loss(y_pred.float(), target.float())}
+        loss = F.mse_loss(y_pred.float(), target.float())
+        return loss
 
-    def validation_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        tensorboard_logs = {'val_loss': avg_loss}
-        return {'avg_val_loss': avg_loss, 'progress_bar': tensorboard_logs}
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack(outputs).mean()
+        self.log('avg_val_loss', avg_loss)
 
     def test_step(self, batch, batch_idx):
         # batch
@@ -64,30 +62,30 @@ class AHS_BERT(pl.LightningModule):
         # fwd
         y_pred = self.forward(input_ids, attention_mask)
         # loss
-        return {'val_loss': F.mse_loss(y_pred.float(), target.float())}
+        loss = F.mse_loss(y_pred.float(), target.float())
+        return loss
 
-    def test_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        tensorboard_logs = {'val_loss': avg_loss}
-        return {'avg_val_loss': avg_loss, 'progress_bar': tensorboard_logs}
+    def test_epoch_end(self, outputs):
+        avg_loss = torch.stack(outputs).mean()
+        self.log('avg_test_loss', avg_loss)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-4)
 
-    def configure_dataloaders(self, input_ids, att_masks, targets, batch_size):
+    def configure_dataloaders(self, input_ids, att_masks, targets, batch_size, shuffle):
         id_tensor = torch.tensor(input_ids)
         att_masks_tensor = torch.tensor(att_masks)
         target_tensor = torch.tensor(targets)
         dataset = TensorDataset(id_tensor, att_masks_tensor,
                                 target_tensor)
         dataloader = DataLoader(dataset, batch_size=batch_size,
-                                shuffle=True)
+                                shuffle=shuffle)
         return dataloader
 
 #Begin Script
 df_all = pd.read_csv("all_data.csv")
 df = df_all[['comment_text', 'toxicity']]
-df = df[:25000]
+df = df[:500000]
 
 x_train, x_temp, y_train, y_temp = train_test_split(df['comment_text'], df['toxicity'], random_state=1111, test_size=0.3)
 x_validation, x_test, y_validation, y_test = train_test_split(x_temp, y_temp, random_state=1111, test_size=0.5)
@@ -113,10 +111,13 @@ model = AHS_BERT()
 
 #Load Datasets
 training_DL = model.configure_dataloaders(tokens_train.data['input_ids'], tokens_train.data['attention_mask'],
-                                           y_train.to_numpy().astype(float), 1)
+                                           y_train.to_numpy().astype(float), 32, shuffle=True)
 validation_DL = model.configure_dataloaders(tokens_validation.data['input_ids'], tokens_validation.data['attention_mask'],
-                                           y_validation.to_numpy().astype(float), 1)
+                                           y_validation.to_numpy().astype(float), 32, shuffle=False)
+test_DL = model.configure_dataloaders(tokens_test.data['input_ids'], tokens_test.data['attention_mask'],
+                                           y_test.to_numpy().astype(float), 32, shuffle=False)
 
 #Begin Lightning Trainer
-trainer = pl.Trainer(gpus=1, max_epochs=1)
+trainer = pl.Trainer(gpus=1, max_epochs=5)
 trainer.fit(model, training_DL, validation_DL)
+trainer.test(dataloaders=test_DL)
